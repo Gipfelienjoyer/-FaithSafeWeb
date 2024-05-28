d13bba856453b1264fd22cd0aff7f4aa7869a4c9
Gipfelienjoyer <luis.haenny@proton.me>
2024-05-28 12:09:19 +0200

    some text

--- src/02-organisms/TopLevelTexts.tsx
+++ src/02-organisms/TopLevelTexts.tsx
@@ -5,7 +5,7 @@ export default function TopLevelTexts() {
     return (
         <Box textAlign="center" width="100%">
             <Typography variant="h4">
-                Some random text to replace
+                Secure your digital life now
             </Typography>
             <Typography variant="h1">
                 Faithsafe
