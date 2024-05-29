b2739831f187ccc64bf0bb8e5f4820c915d5ae43
Lon60 <leon.liechti@icloud.com>
2024-05-29 22:42:19 +0200

    atomize

--- src/03-templates/HomeContentTmpl.tsx
+++ src/03-templates/HomeContentTmpl.tsx
@@ -1,18 +1,16 @@
 import { Grid } from "@mui/material";
-import HappyCustomer1 from "../02-organisms/HappyCustomer1";
+import ListImage from "../02-organisms/ListImage";
 import WhyFaithSafeContent from "../01-molecules/WhyFaithSafeContent";
 import PicAndContent from "../02-organisms/PicAndContent";
+import happyCustomer1 from '../05-assets/happyCustomer1.png';
+
 
 export default function HomeContentTmpl() {
     return (
         <Grid container spacing={2}>
             <PicAndContent>
-                <HappyCustomer1 />
+                <ListImage imgAlt={"Happy Customer"} imgSrc={happyCustomer1}/>
                 <WhyFaithSafeContent />
-            </PicAndContent>
-            <PicAndContent>
-                <WhyFaithSafeContent />
-                <HappyCustomer1 />
             </PicAndContent>
         </Grid>
     );
